e71ffb69a290afc711fc66b7702a86cb96b2e916
technology <technology@CPX-28TSKKLG0HH.dir.svc.accenture.com>
2019-05-21 20:28:46 -0500

    inicio del proyecto

---
+++ Tecnoloshe.Git/Tecnoloshe.Git/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tecnoloshe.Git
+{
+    public partial class Form1 : Form
+    {
+        private int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Tareas tarea = new Tareas();
+            long n= tarea.num();
+            label1.Text = n.ToString();
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            contador += 1;
+            label2.Text = contador.ToString();
+
+        }
+    }
+}
---
+++ Tecnoloshe.Git/Tecnoloshe.Git/Tareas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnoloshe.Git
+{
+    public class Tareas
+    {
+        long numero = 0;
+        public long num()
+        {
+            for (long i=0;i <= 1000000; i++)
+            {
+                numero += i;
+            }
+            return numero;
+        }
+    }
+}
